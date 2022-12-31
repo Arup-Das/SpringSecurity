957e8acaafafbc6726f2dad9264b5d191cd1ec83
ArupKDas <akdcse2k10@gmail.com>
2022-12-31 20:51:45 +0600

    Spring security basic

---
+++ SpringSecurityBasic/src/main/java/com/arup/springsecuritybaisc/SpringSecurityBaiscApplication.java
@@ -0,0 +1,13 @@
+package com.arup.springsecuritybaisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaiscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBaiscApplication.class, args);
+    }
+
+}
---
+++ SpringSecurityBasic/src/test/java/com/arup/springsecuritybaisc/SpringSecurityBaiscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arup.springsecuritybaisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaiscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
